1fc202125facaf69324f022f1b65484b2d40761d
Deletron247 <danil.dmitrievn@gmail.com>
2024-02-28 20:39:56 +0300

    Переписан videocdn

--- application/libs/videocdn.py
+++ application/libs/videocdn.py
@@ -1,10 +1,10 @@
 import json
 import re
-
-# import config as config
 import requests
 from bs4 import BeautifulSoup
-import application.models.Common as Common
+
+
+CACHE_TIME = 3600
 
 
 class videocdnException(Exception):
@@ -12,28 +12,25 @@ class videocdnException(Exception):
 
 
 class VideoCDN:
-    def __init__(self):
+    def __init__(self, api_key: str):
         self.HEADERS = {
             "referer": "https://videocdn.tv/",
             "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
         }
-        self.api_key = Common.CONFIG["VIDEOCDN_API"]
+        self.api_key = api_key
 
-    def getUrl(self, url):
-        self.url = url
-        self.page = self.getPage()
-        self.soup = self.getSoup()
-        return self.getVideo()
+    def setKPid(self, kp_id):
+        self.kp_id = str(kp_id)
+        self.data = self.getData()
 
-    def getKPid(self, kp_id):
+    def getData(self):
         if self.api_key == "":
             raise videocdnException("Error: VideoCDN API key is not set.")
-        self.url = f"https://videocdn.tv/api/short?api_token={self.api_key}&kinopoisk_id={kp_id}"
+        self.url = f"https://videocdn.tv/api/short?api_token={self.api_key}&kinopoisk_id={self.kp_id}"
         response_data = json.loads(self.getPage().text)
         self.url = f"http:{response_data['data'][0]['iframe_src']}"
         self.page = self.getPage()
         self.soup = self.getSoup()
-        return self.getVideo()
 
     def getPage(self):
         for _ in range(3):
@@ -50,79 +47,95 @@ class VideoCDN:
     def getSoup(self):
         return BeautifulSoup(self.page.text, "html.parser")
 
-    def getVideo(self):
-        """
-        This function gets the video URLs from a given page.
-        """
-        # Get the files from the page
-        downloads = self.soup.find(id="downloads")
-        files = downloads.attrs["value"]
+    def URLconvertStrToList(self, string):
+        # Split the URLs and create a dictionary with placeholders
+        url_list = string.split(",")
+        videos_url = {}
+        for url in url_list:
+            resolution = url.split("//")[0].replace("[", "").replace("p]", "")
+            video_url = "http://" + url.split("//")[1]
+            videos_url[resolution] = video_url
+        return videos_url
 
-        # Get the translations from the page
+    def Movie_link(self) -> dict:
+        data = {}
+        download = self.soup.select_one("#fs")
         translations = self.soup.find(class_="translations")
-
-        # Replace the backslashes with forward slashes
-        files = files.replace("\/", "/")
-
-        # Load the files as JSON
-        files = json.loads(files)
-
-        data = []
-
-        # Extract translation names
-        translation_name = ""
         if translations:
             translation = {}
             for option in translations.find_all("option"):
                 translation[option.attrs["value"]] = option.get_text().strip()
-            translation_name = translation
+        if download:
+            json_data = download.get("value")
+            if json_data:
+                json_data = json.loads(json_data)
+                for translation_id, translation_data in json_data.items():
+                    # Add the translation name to the dictionary
+                    data[translation[translation_id]] = self.URLconvertStrToList(
+                        translation_data
+                    )
+        return data
 
-        # Loop through the files
-        for file_id, file_data in files.items():
-            urls = {}
-            for key, video_file in file_data.items():
-                # If the translation name is empty, set it to the video file
-                if translation_name == "":
-                    match = re.search(r"dn=(.*?)\[(.*?)\]", str(video_file))
-                    if match:
-                        translation_name = match.group(2)
-                # Add the URL to the list of URLs
-                if key in ["1080p", "720p", "480p", "360p", "240p"]:
-                    urls[key] = f"https:{video_file}"
-                else:
-                    # Add https to url serial
-                    for series in video_file:
-                        for video in video_file[series]:
-                            video_file[series][
-                                video
-                            ] = f"https:{video_file[series][video]}"
-                    urls[key] = video_file
+    def TvSeasons(self) -> dict:
+        seasons_data = []
+        try:
+            download = self.soup.select_one("#fs")
+            if download:
+                json_data = download.get("value")
+                if json_data:
+                    json_data = json.loads(json_data)
+                    for translation_id, translation_data in json_data.items():
+                        if translation_id == "0":
+                            continue
+                        translation_name = (
+                            translation_data[0]["folder"][0]["comment"]
+                            .split("<br><i>")[1]
+                            .split("</i>")[0]
+                        )
+                        seasons = [
+                            {
+                                translation["id"]: [
+                                    video["id"].split("_")[1]
+                                    for video in translation["folder"]
+                                ]
+                            }
+                            for translation in translation_data
+                        ]
+                        seasons_data.append(
+                            {"name": translation_name, "seasons": seasons}
+                        )
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+        return seasons_data
 
-            # Append the data to the list
-            if translations:
-                data.append(
-                    {
-                        # "id": file_id,
-                        "translation_name": translation[file_id],
-                        "files": urls,
-                    }
-                )
-            else:
-                data.append(
-                    {
-                        # "id": file_id,
-                        "translation_name": translation_name,
-                        "files": urls,
-                    }
-                )
-                break
-
-        # Return the data
+    def TV_link(self, season, series) -> dict:
+        data = {}
+        download = self.soup.select_one("#fs")
+        if download:
+            json_data = download.get("value")
+            if json_data:
+                json_data = json.loads(json_data)
+                for translation_id, translation_data in json_data.items():
+                    for season_data in translation_data:
+                        for folder_data in season_data["folder"]:
+                            if folder_data["id"] == f"{season}_{series}":
+                                translation_name = (
+                                    folder_data["comment"]
+                                    .split("<br><i>")[1]
+                                    .split("</i>")[0]
+                                )
+                                data[translation_name] = self.URLconvertStrToList(
+                                    folder_data["file"]
+                                )
+                                break
         return data
 
 
 if __name__ == "__main__":
-    data = VideoCDN()
-    # print(data.getUrl("https://68175.svetacdn.in/Z1VVUcly7Aoi/tv-series/23"))
-    print(data.getKPid("4647040"))
-    # print(data.getKPid("994676"))
+    data = VideoCDN("Api_key")
+    # data.setKPid("491724")
+    # print(data.Movie_link())
+
+    data.setKPid("749374")
+    # print(data.TvSeasons())
+    print(data.TV_link(1, 1))
--- application/models/Extractor.py
+++ application/models/Extractor.py
@@ -1,9 +1,6 @@
-from application.libs.voidboost import voidboost
 from application.libs.videocdn import VideoCDN
-
-# from urllib.parse import urlencode
-# import requests
-# import application.models.Common as Common
+from application.libs.voidboost import voidboost
+import application.models.Common as Common
 
 
 class Extractor_Model:
@@ -24,8 +21,8 @@ class Extractor_Model:
             data["voidboost"] = voidboost_data
         if player_name == "videocdn" or player_name == "all":
             videocdn_data = {}
-            videocdn = VideoCDN()
-            for video in videocdn.getKPid(kp_id):
+            videocdn = VideoCDN(kp_id, Common.CONFIG["VIDEOCDN_API"])
+            for video in videocdn.getData():
                 videocdn_data[video["translation_name"]] = video["files"]
             data["videocdn"] = videocdn_data
         return data
@@ -34,10 +31,10 @@ class Extractor_Model:
         data = {}
         if player_name == "voidboost" or player_name == "all":
             void = voidboost(kp_id)
-            data["voidboost"] = void.get_seasons()
+            data["voidboost"] = void.getSeasons()
         if player_name == "videocdn" or player_name == "all":
-            videocdn = VideoCDN()
-            data["videocdn"] = videocdn.getKPid(kp_id)
+            videocdn = VideoCDN(kp_id, Common.CONFIG["VIDEOCDN_API"])
+            data["videocdn"] = videocdn.getSeasons()
         return data
 
     def get_player_tvseries(self, kp_id, player_name, season, series):
@@ -45,67 +42,13 @@ class Extractor_Model:
         if player_name == "voidboost" or player_name == "all":
             voidboost_data = {}
             void = voidboost(kp_id)
-            seasons_data = void.get_seasons()
+            seasons_data = void.getSeasons(v_key=True)
             for season_data in seasons_data:
                 voidboost_data[season_data["name"]] = void.get_series_stream(
                     season_data["video_key"], season, series
                 )
             data["voidboost"] = voidboost_data
+        if player_name == "videocdn" or player_name == "all":
+            videocdn = VideoCDN(kp_id, Common.CONFIG["VIDEOCDN_API"])
+            data["videocdn"] = videocdn.getVideos()
         return data
-
-    # # $query = ['imdb_id' => 'tt2570292', 'limit'=> 1]; //поиск по imdb_id
-    # # $query = ['title' => 'Аватар', 'year' => 2004]; //поиск по title
-    # # $query = ['kinopoisk_id' => '1009278', 'limit'=> 1]; //поиск по kp_id
-    # # $query = ['shikimori_id' => '39247', 'limit'=> 1]; //поиск по shikimori_id
-    # # kodik_fieldch(query={"imdb_id": "tt2570292", "year": 2004})
-    # def kodik_fieldch(self, query):
-    #     req = requests.get(
-    #         "https://kodikapi.com/fieldch?token=" + KODIK_API + "&" + urlencode(query)
-    #     )
-    #     json_data = req.json_data()
-    #     if json_data is not None or json_data != "":
-    #         if json_data["total"] != 0:
-    #             json_data["status"] = "true"
-    #             return json_data
-    #         else:
-    #             return {"status": "false"}
-    #     else:
-    #         return {"status": "false"}
-
-    # # $query = ['query' => tt1798268, 'field' => 'imdb_id']; поиск по imdb_id
-    # # $query = ['query' => 50598, 'field' => 'kinopoisk_id']; поиск по kpid
-    # # $query = ['query' => 'герой', 'year' => 2002]; поиск по названию и году
-    # # videocdn_fieldch({"query": "Спасатель", "field": "ru_title"}, "movie")
-    # def videocdn_fieldch(self, query, type_data):
-    #     if type_data == "movie":
-    #         # Check for movies
-    #         types = ["movies", "animes"]
-    #     else:
-    #         # Check for TV series
-    #         types = ["anime-tv-series", "show-tv-series", "tv-series"]
-
-    #     for type in types:
-    #         data = self.videocdn_get(query, type)
-    #         if data.get("status") == "true":
-    #             return data
-
-    #     return None
-
-    # # делает запрос находит совпадение и возвращает ответ
-    # def videocdn_get(self, query, type):
-    #     url = "https://videocdn.tv/api/{}?api_token={}&{}".format(
-    #         type, VIDEOCDN_API, urlencode(query)
-    #     )
-    #     results = requests.get(url)
-    #     json_data = results.json()
-
-    #     response = {"status": "false"}
-    #     if "data" in json_data and json_data["data"] is not None:
-    #         for item in json_data["data"]:
-    #             if Common.normalize(item[query["field"]]) == Common.normalize(
-    #                 query["query"]
-    #             ):
-    #                 response["status"] = "true"
-    #                 response["data"] = item
-    #                 break
-    #     return response
